0cdb25f14e237d771d1d822c7eb9b4c09f8cc328
Liang.Xiong <137010393@qq.com>
2020-04-14 16:28:51 +0800

    添加

---
+++ src/main/java/com/java/common/App.java
@@ -0,0 +1,7 @@
+package com.java.common;
+
+/**
+ * Created by Administrator on 2020/4/14.
+ */
+public class App {
+}
---
+++ src/main/java/com/java/common/utils/App.java
@@ -0,0 +1,7 @@
+package com.java.common.utils;
+
+/**
+ * Created by Administrator on 2020/4/14.
+ */
+public class App {
+}
